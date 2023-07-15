e08ebc084a9a83f221c2d20b2fd11110a9848a53
David Stacy <davidstacy@users.noreply.github.com>
2023-07-15 17:43:06 -0400

    first pass at git hosted deployer scripts

--- prepare-cluster.sh
+++ prepare-cluster.sh
@@ -1,14 +1,14 @@
 # install OpenShift GitOps
-oc apply -k k8s/setup/argocd-operator --insecure-skip-tls-verify=true
+oc apply -k https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/argocd-operator --insecure-skip-tls-verify=true
 while ! oc wait crd applications.argoproj.io --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
 while ! oc wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n openshift-gitops  --insecure-skip-tls-verify=true > /dev/null; do sleep 30; done
-oc apply -k k8s/setup/argocd-instance --insecure-skip-tls-verify=true
+oc apply -k https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/argocd-instance --insecure-skip-tls-verify=true
 
 # install OpenShift Pipelines
-oc apply -k k8s/openshift-pipelines-operator --insecure-skip-tls-verify=true
+oc apply -k https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/openshift-pipelines-operator --insecure-skip-tls-verify=true
 
 # install external secrets operator
-oc apply -k k8s/externalsecrets-operator --insecure-skip-tls-verify=true
+oc apply -k https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/externalsecrets-operator --insecure-skip-tls-verify=true
 while ! oc wait crd clustersecretstores.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
 while ! oc wait crd externalsecrets.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
 while ! oc wait crd operatorconfigs.operator.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true 2>/dev/null; do sleep 30; done
