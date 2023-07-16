ddc71a2400d4f9daa3edb6719925551e36d0c0af
David Stacy <davidstacy@users.noreply.github.com>
2023-07-16 14:09:35 -0400

    try remote url

--- prepare-cluster.sh
+++ prepare-cluster.sh
@@ -1,13 +1,14 @@
 
 # get manifests
-wget -qO - https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/argocd-operator > argocd-operator.yaml
-wget -qO - https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/argocd-instance > argocd-instance.yaml
-wget -qO - https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/openshift-pipelines-operator > openshift-pipelines-operator.yaml
-wget -qO - https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/externalsecrets-operator > externalsecrets-operator.yaml
-wget -qO - https://raw.githubusercontent.com/cloud-native-toolkit/deployer-tekton-tasks/main/argocd.yaml > tasks.yaml
-
+#wget -qO - https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/argocd-operator > argocd-operator.yaml
+#wget -qO - https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/argocd-instance > argocd-instance.yaml
+#wget -qO - https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/openshift-pipelines-operator > openshift-pipelines-operator.yaml
+#wget -qO - https://raw.githubusercontent/cloud-native-toolkit/deployer-cluster-prep/externalsecrets-operator > externalsecrets-operator.yaml
+#wget -qO - https://raw.githubusercontent.com/cloud-native-toolkit/deployer-tekton-tasks/main/argocd.yaml > tasks.yaml
+#https://github.com/cloud-native-toolkit/deployer-cluster-prep//argocd-operator/?ref=main
+#https://github.com/kubernetes-sigs/kustomize//examples/multibases/dev/?ref=v1.0.6
 # install OpenShift GitOps
-oc apply -k argocd-operator --insecure-skip-tls-verify=true
+oc apply -k https://github.com/cloud-native-toolkit/deployer-cluster-prep//argocd-operator/?ref=main --insecure-skip-tls-verify=true
 while ! oc wait crd applications.argoproj.io --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
 while ! oc wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n openshift-gitops  --insecure-skip-tls-verify=true > /dev/null; do sleep 30; done
 oc apply -k argocd-instance --insecure-skip-tls-verify=true
