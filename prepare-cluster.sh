ff844e8fd99e9d58a7bae03033a2e04b2a80886a
David Stacy <davidstacy@users.noreply.github.com>
2023-07-17 07:59:11 -0400

    Update prepare-cluster.sh

--- prepare-cluster.sh
+++ prepare-cluster.sh
@@ -16,21 +16,25 @@ oc apply -k https://github.com/cloud-native-toolkit/deployer-cluster-prep//argoc
 
 # install OpenShift Pipelines
 oc apply -k https://github.com/cloud-native-toolkit/deployer-cluster-prep//openshift-pipelines-operator/?ref=main --insecure-skip-tls-verify=true
-sleep 5
+while ! oc wait crd pipelines.tekton.dev --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
+while ! oc wait crd pipelineruns.tekton.dev --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
+while ! oc wait crd tasks.tekton.dev --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
+while ! oc wait --for=condition=ready TektonPipelines/pipeline --timeout=-1s --insecure-skip-tls-verify=true 2>/dev/null; do sleep 30; done
+
 
 # install external secrets operator
 oc apply -k https://github.com/cloud-native-toolkit/deployer-cluster-prep//externalsecrets-operator/?ref=main --insecure-skip-tls-verify=true
-sleep 5
 while ! oc wait crd clustersecretstores.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
 while ! oc wait crd externalsecrets.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
 while ! oc wait crd operatorconfigs.operator.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true 2>/dev/null; do sleep 30; done
 
-# Give default:pipeline SA cluster-admin permissions
+# Give default:pipeline SA cluster-admin permissions 
 oc create clusterrolebinding pipeline-clusteradmin-crb --clusterrole=cluster-admin --serviceaccount=default:pipeline --insecure-skip-tls-verify=true
 sleep 5
 
 # Add deployer tekton tasks to cluster in the default namespace
 oc apply -f https://raw.githubusercontent.com/cloud-native-toolkit/deployer-tekton-tasks/main/argocd.yaml --insecure-skip-tls-verify=true
+sleep 5
 
 #patch storage class for a default
 oc patch storageclass ocs-storagecluster-cephfs  -p '{"metadata": {"annotations": {"storageclass.kubernetes.io/is-default-class": "true"}}}' || true
