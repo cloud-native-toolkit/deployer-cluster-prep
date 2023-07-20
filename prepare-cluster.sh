58852fa7621253b51d2209977aa88a4541dce275
David Stacy <davidstacy@users.noreply.github.com>
2023-07-19 23:54:16 -0400

    fix bug

--- prepare-cluster.sh
+++ prepare-cluster.sh
@@ -8,37 +8,25 @@ set -x
 
 # install OpenShift GitOps
 oc apply -k https://github.com/cloud-native-toolkit/deployer-cluster-prep//argocd-operator/?ref=main --insecure-skip-tls-verify=true
-sleep 5
-#while ! oc wait crd applications.argoproj.io --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
-oc wait crd applications.argoproj.io --timeout=1800s --for=condition=Established  --insecure-skip-tls-verify=true
-sleep 5
-#while ! oc wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n openshift-gitops  --insecure-skip-tls-verify=true > /dev/null; do sleep 30; done
-oc wait pod -l '!job-name' -n openshift-gitops --timeout=1800s --for=condition=Ready  --insecure-skip-tls-verify=true
+while ! oc wait crd applications.argoproj.io --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true 2>/dev/null; do sleep 30; done
+while ! oc wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n openshift-gitops  --insecure-skip-tls-verify=true 2>/dev/null; do sleep 30; done
 
 oc apply -k https://github.com/cloud-native-toolkit/deployer-cluster-prep//argocd-instance/?ref=main --insecure-skip-tls-verify=true
 
 # install OpenShift Pipelines
 oc apply -k https://github.com/cloud-native-toolkit/deployer-cluster-prep//openshift-pipelines-operator/?ref=main --insecure-skip-tls-verify=true
-#while ! oc wait crd pipelines.tekton.dev --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
-#while ! oc wait crd pipelineruns.tekton.dev --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
-#while ! oc wait crd tasks.tekton.dev --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
-#while ! oc wait --for=condition=ready TektonPipelines/pipeline --timeout=-1s --insecure-skip-tls-verify=true 2>/dev/null; do sleep 30; done
-#while ! oc wait --for=condition=Ready TektonAddon/addon  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 10; done
-oc wait crd pipelines.tekton.dev --timeout=1800s --for=condition=Established  --insecure-skip-tls-verify=true
-oc wait crd pipelineruns.tekton.dev --timeout=1800s --for=condition=Established  --insecure-skip-tls-verify=true
-oc wait crd tasks.tekton.dev --timeout=1800s --for=condition=Established  --insecure-skip-tls-verify=true
-oc wait TektonPipelines/pipeline --for=condition=ready  --timeout=1800s --insecure-skip-tls-verify=true
-oc wait TektonAddon/addon --for=condition=Ready  --timeout=1800s --insecure-skip-tls-verify=true
+while ! oc wait crd pipelines.tekton.dev --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
+while ! oc wait crd pipelineruns.tekton.dev --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
+while ! oc wait crd tasks.tekton.dev --timeout=-1s --for=condition=Established  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
+while ! oc wait --for=condition=ready TektonPipelines/pipeline --timeout=-1s --insecure-skip-tls-verify=true 2>/dev/null; do sleep 30; done
+while ! oc wait --for=condition=Ready TektonAddon/addon  --insecure-skip-tls-verify=true  2>/dev/null; do sleep 10; done
 
 
 # install external secrets operator
 oc apply -k https://github.com/cloud-native-toolkit/deployer-cluster-prep//externalsecrets-operator/?ref=main --insecure-skip-tls-verify=true
-#while ! oc wait crd clustersecretstores.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
-#while ! oc wait crd externalsecrets.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
-#while ! oc wait crd operatorconfigs.operator.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true 2>/dev/null; do sleep 30; done
-oc wait crd clustersecretstores.external-secrets.io --timeout=1800s --for=condition=Established --insecure-skip-tls-verify=true
-oc wait crd externalsecrets.external-secrets.io --timeout=1800s --for=condition=Established --insecure-skip-tls-verify=true
-oc wait crd operatorconfigs.operator.external-secrets.io --timeout=1800s --for=condition=Established --insecure-skip-tls-verify=true
+while ! oc wait crd clustersecretstores.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
+while ! oc wait crd externalsecrets.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true  2>/dev/null; do sleep 30; done
+while ! oc wait crd operatorconfigs.operator.external-secrets.io --timeout=-1s --for=condition=Established --insecure-skip-tls-verify=true 2>/dev/null; do sleep 30; done
 
 # Give default:pipeline SA cluster-admin permissions 
 oc create clusterrolebinding pipeline-clusteradmin-crb --clusterrole=cluster-admin --serviceaccount=default:pipeline --insecure-skip-tls-verify=true
