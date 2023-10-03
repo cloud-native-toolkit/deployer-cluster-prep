710c68adc98be924fe1b2072c8c6afdfd6316d21
Craig Cooper <craigcooper87911@gmail.com>
2023-10-03 15:07:53 -0500

    remove comment

--- prepare-cluster.sh
+++ prepare-cluster.sh
@@ -30,7 +30,7 @@ while ! oc wait crd externalsecrets.external-secrets.io --timeout=1800s --for=co
 while ! oc wait crd operatorconfigs.operator.external-secrets.io --timeout=1800s --for=condition=Established --insecure-skip-tls-verify=true; do sleep 30; done
 
 # Give default:pipeline SA cluster-admin permissions 
-# oc create clusterrolebinding pipeline-clusteradmin-crb --clusterrole=cluster-admin --serviceaccount=default:pipeline --insecure-skip-tls-verify=true
+oc create clusterrolebinding pipeline-clusteradmin-crb --clusterrole=cluster-admin --serviceaccount=default:pipeline --insecure-skip-tls-verify=true
 
 # Add deployer tekton tasks to cluster in the default namespace
 oc apply -f https://raw.githubusercontent.com/cloud-native-toolkit/deployer-tekton-tasks/main/argocd.yaml --insecure-skip-tls-verify=true
